22af4488be92a229fb1529df028933d305f425a7
Nicholas-Tuttle <44779782+Nicholas-Tuttle@users.noreply.github.com>
2023-02-26 16:09:51 -0600

    Moving all materials into 1 file

--- RayTracerLib/GPUMaterialCalculator.cpp
+++ RayTracerLib/GPUMaterialCalculator.cpp
@@ -2,6 +2,8 @@
 
 using RayTracer::GPUMaterialCalculator;
 
+#pragma region Material Calculator
+
 GPUMaterialCalculator::GPUMaterialCalculator(vk::Device device)
 	: device(device), diffuse_material(device), world_material(device)
 {
@@ -19,3 +21,42 @@ void GPUMaterialCalculator::Execute(uint32_t compute_queue_index,
 	world_material.Execute(compute_queue_index, incoming_ray_count, input_gpu_intersection_buffer, output_gpu_ray_buffer);
 	diffuse_material.Execute(compute_queue_index, incoming_ray_count, input_gpu_intersection_buffer, output_gpu_ray_buffer, input_diffuse_material_parameters);
 }
+
+#pragma endregion
+
+#pragma region World Material
+
+GPUMaterialCalculator::GPUWorldMaterial::GPUWorldMaterial(vk::Device device)
+	: WorldMaterialPushConstants(), GPUComputeShader("GPUWorldMaterial.comp.spv", 2, sizeof(GPUWorldMaterial::WorldMaterialPushConstants), device)
+{
+#ifdef _DEBUG
+	std::cout << __FUNCTION__ << std::endl;
+#endif
+}
+
+void GPUMaterialCalculator::GPUWorldMaterial::Execute(uint32_t compute_queue_index, size_t incoming_ray_count,
+	vk::Buffer input_gpu_intersection_buffer, vk::Buffer output_gpu_ray_buffer)
+{
+	GPUComputeShader::Execute(compute_queue_index, incoming_ray_count, std::vector<vk::Buffer>{input_gpu_intersection_buffer, output_gpu_ray_buffer}, static_cast<void *>(&WorldMaterialPushConstants));
+}
+
+#pragma endregion
+
+#pragma region Diffuse Material
+
+GPUMaterialCalculator::GPUDiffuseMaterial::GPUDiffuseMaterial(vk::Device device)
+	: DiffuseMaterialPushConstants(), GPUComputeShader("GPUDiffuseMaterial.comp.spv", 3, sizeof(GPUDiffuseMaterial::DiffuseMaterialPushConstants), device)
+{
+#ifdef _DEBUG
+	std::cout << __FUNCTION__ << "\n";
+#endif
+}
+
+void GPUMaterialCalculator::GPUDiffuseMaterial::Execute(uint32_t compute_queue_index, size_t incoming_ray_count,
+	vk::Buffer input_gpu_intersection_buffer, vk::Buffer output_gpu_ray_buffer, vk::Buffer input_gpu_material_parameters)
+{
+	GPUComputeShader::Execute(compute_queue_index, incoming_ray_count, std::vector<vk::Buffer>{input_gpu_intersection_buffer, output_gpu_ray_buffer, input_gpu_material_parameters}, static_cast<void *>(&DiffuseMaterialPushConstants));
+}
+
+#pragma endregion
+
--- include/GPUMaterialCalculator.h
+++ include/GPUMaterialCalculator.h
@@ -2,8 +2,8 @@
 
 #include <vulkan/vulkan.hpp>
 #include <iostream>
-#include "GPUWorldMaterial.h"
-#include "GPUDiffuseMaterial.h"
+#include "GPUComputeShader.h"
+#include "GPURenderer.h"
 
 namespace RayTracer
 {
@@ -20,7 +20,50 @@ namespace RayTracer
 	private:
 		vk::Device device;
 
-		GPUDiffuseMaterial diffuse_material;
+		class GPUWorldMaterial : protected GPUComputeShader
+		{
+		public:
+			GPUWorldMaterial(vk::Device device);
+			void Execute(uint32_t compute_queue_index, size_t incoming_ray_count,
+				vk::Buffer input_gpu_intersection_buffer, vk::Buffer output_gpu_ray_buffer);
+		private:
+			GPUWorldMaterial(const GPUWorldMaterial &other) = delete;
+			GPUWorldMaterial(const GPUWorldMaterial &&other) = delete;
+
+			struct push_constants
+			{
+				push_constants()
+				{
+					material_id = static_cast<uint32_t>(GPURenderer::MaterialTypeID::world);
+				}
+
+				uint32_t material_id;
+			} WorldMaterialPushConstants;
+		};
+
+		class GPUDiffuseMaterial : protected GPUComputeShader
+		{
+		public:
+			GPUDiffuseMaterial(vk::Device device);
+			void Execute(uint32_t compute_queue_index, size_t incoming_ray_count,
+				vk::Buffer input_gpu_intersection_buffer, vk::Buffer output_gpu_ray_buffer,
+				vk::Buffer input_gpu_material_parameters);
+		private:
+			GPUDiffuseMaterial(const GPUDiffuseMaterial &other) = delete;
+			GPUDiffuseMaterial(const GPUDiffuseMaterial &&other) = delete;
+
+			struct push_constants
+			{
+				push_constants()
+				{
+					material_id = static_cast<uint32_t>(GPURenderer::MaterialTypeID::diffuse);
+				}
+
+				uint32_t material_id;
+			} DiffuseMaterialPushConstants;
+		};
+
 		GPUWorldMaterial world_material;
+		GPUDiffuseMaterial diffuse_material;
 	};
 }
